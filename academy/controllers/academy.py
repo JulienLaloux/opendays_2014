53089469c6f90b88ddad17d06232491742b740c0
openerp <openerp@openerp-dev.(none)>
2014-06-02 15:58:45 +0200

    Detailed page by teacher assistant works and remove some commented code

--- academy/controllers/academy.py
+++ academy/controllers/academy.py
@@ -3,12 +3,6 @@
 from openerp import http
 from openerp.addons.web.controllers import main
 
-# teaching_assistants = [
-#     {'name': 'Joe', 'age': 18},
-#     {'name': 'Ellen', 'age': 17},
-#     {'name': 'Ed', 'age': 18},
-# ]
-
 class academy(main.Home):
 
     @http.route('/', auth='public', website=True)
@@ -17,15 +11,8 @@ class academy(main.Home):
         TAs = http.request.registry('academy.ta')
         tas = TAs.search_read(cr, uid, [], context=context)
         return http.request.render('academy.index', {'tas': tas})
-        # return ''.join(
-        #     '<li><a href="/tas/%d/">%s</a></li>' % (index, ta['name'])
-        #     for index, ta in enumerate(teaching_assistants)
-        # )
 
-    @http.route('/tas/<int:index>/', auth='public', website=True)
-    def ta(self, index):
-        cr, uid, context = http.request.cr, http.request.uid, http.request.context
-        TAs = http.request.registry('academy.ta')
-        tas = TAs.read(cr, uid, index, context=context)
-        return http.request.render('academy.ta', tas)
-        # return "<p>%(name)s is %(age)d</p>" % teaching_assistants[index]
+    ## The name after the 2 points must be the same same as in the function call. Keyword must be the same
+    @http.route('/tas/<model("academy.ta"):record>/', auth='public', website=True)
+    def ta(self, record):
+        return http.request.render('academy.ta', {'ta': record})
--- academy/models/academy.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import Model
-from openerp import Char
-
-class academy(Model):
-    _name = "academy.academy"
-
-    name = Char()
