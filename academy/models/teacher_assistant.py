0093b314ebb2f7fd66c6e62c89aaf2734ece5c3b
openerp <openerp@openerp-dev.(none)>
2014-06-02 17:01:51 +0200

    Add an HTML field and add a view in the back end(for personal test)

--- academy/models/teacher_assistant.py
+++ academy/models/teacher_assistant.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from openerp import Model
-from openerp import Char, Integer
+from openerp import Char, Integer, Html
 
 class teacher(Model):
     _name = "academy.ta"
 
     name = Char()
     age = Integer()
+    biography = Html()
