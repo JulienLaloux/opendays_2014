db608d388fe11cd6e0fd78e8e438a24b06801aef
openerp <openerp@openerp-dev.(none)>
2014-06-03 10:27:33 +0200

    Add a Simplify option in the Customize menu (and remove pyc)

--- academy/__openerp__.py
+++ academy/__openerp__.py
@@ -20,7 +20,7 @@
         'website_event'
     ],
     'data': [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'templates/academy.xml',
         'templates/website_event.xml',
         'data/teacher_assistant.xml',
